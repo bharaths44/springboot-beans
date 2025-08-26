d0d4b2f3c1dfd22dcaa568617c2415c67f23f94e
bharaths44 <bharathsatheesan@gmail.com>
2025-08-26 21:54:31 +0530

    @RequestParam demo

--- src/main/java/com/bharath/demo/FirstController.java
+++ src/main/java/com/bharath/demo/FirstController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
-    }
+    // @GetMapping("/hello")
+    // public String sayHello() {
+    // return "Hello, World!";
+    // }
 
     @PostMapping("/post")
     public String post(@RequestBody String message) {
@@ -25,9 +25,17 @@ public class FirstController {
         return "Posted message: " + order.toString();
     }
 
-    @GetMapping("/hello/{user-name}")
-    public String pathVar(@PathVariable("user-name") String userName) {
-        return "Hello " + userName;
+    // http://localhost:8080/hello/alibou
+    // @GetMapping("/hello/{user-name}")
+    // public String pathVar(@PathVariable("user-name") String userName) {
+    // return "Hello " + userName;
+    // }
+
+    // http://localhost:8080/hello?param_name=paramvalue&param_name_2=value_2
+    @GetMapping("/hello")
+    public String paramVar(@RequestParam("user-name") String userName,
+            @RequestParam("user-lastname") String lastName) {
+        return "Hello " + userName + " " + lastName;
     }
 
 }
