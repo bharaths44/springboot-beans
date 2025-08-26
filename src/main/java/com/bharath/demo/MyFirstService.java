552825982e08985d4db0a1771b4c0147c8f47976
bharaths44 <bharathsatheesan@gmail.com>
2025-08-26 11:55:39 +0530

    Method Injection

--- src/main/java/com/bharath/demo/MyFirstService.java
+++ src/main/java/com/bharath/demo/MyFirstService.java
@@ -1,6 +1,5 @@
 package com.bharath.demo;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,9 +7,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstService {
     @Autowired
-    private  HelloClass helloClass;
+    @Qualifier("bean1")
+    private HelloClass helloClass;
 
-
+    @Autowired
+    public void injectDependency(@Qualifier("mySecondBean") HelloClass helloClass) {
+        this.helloClass = helloClass;
+    }
 
     public String tellAStory() {
         return "the dependency is saying: " + helloClass.sayHello();
