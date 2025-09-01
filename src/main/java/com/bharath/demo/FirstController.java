fac68ecbd8b7e4bc389162785e1b1f18eff95eb1
bharaths44 <bharathsatheesan@gmail.com>
2025-09-01 11:29:29 +0530

    Connecting to postgres

--- src/main/java/com/bharath/demo/FirstController.java
+++ src/main/java/com/bharath/demo/FirstController.java
@@ -5,37 +5,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class FirstController {
 
-    // @GetMapping("/hello")
-    // public String sayHello() {
-    // return "Hello, World!";
-    // }
+     @GetMapping("/hello")
+     public String sayHello() {
+     return "Hello, World!";
+     }
 
-    @PostMapping("/post")
-    public String post(@RequestBody String message) {
-        return "Posted message: " + message;
-    }
-
-    @PostMapping("/post-order")
-    public String post(@RequestBody Order order) {
-        return "Posted message: " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String postRecord(@RequestBody OrderRecord order) {
-        return "Posted message: " + order.toString();
-    }
-
-    // http://localhost:8080/hello/alibou
-    // @GetMapping("/hello/{user-name}")
-    // public String pathVar(@PathVariable("user-name") String userName) {
-    // return "Hello " + userName;
-    // }
-
-    // http://localhost:8080/hello?param_name=paramvalue&param_name_2=value_2
-    @GetMapping("/hello")
-    public String paramVar(@RequestParam("user-name") String userName,
-            @RequestParam("user-lastname") String lastName) {
-        return "Hello " + userName + " " + lastName;
-    }
 
 }
--- src/main/java/com/bharath/demo/OrderRecord.java
+++
@@ -1,5 +0,0 @@
-package com.bharath.demo;
-
-public record OrderRecord(String customerName, String productName, int quantity) {
-
-}
