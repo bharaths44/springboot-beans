e5be48e8445c1fe82236b5f5d933fe1b72ca807c
bharaths44 <bharathsatheesan@gmail.com>
2025-09-02 21:04:44 +0530

    Add null check in StudentMapper and enhance tests for mapping methods

--- src/test/java/com/bharath/demo/student/StudentMapperTest.java
+++ src/test/java/com/bharath/demo/student/StudentMapperTest.java
@@ -6,15 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StudentMapperTest {
 
-	@BeforeEach
-	void setUp() {
-		System.out.println("Starting TestMethod");
-	}
-
-	@AfterEach
-	void tearDown() {
-		System.out.println("Finished TestMethod");
-	}
+	private StudentMapper mapper;
 
 	@BeforeAll
 	static void beforeAll() {
@@ -26,10 +18,49 @@ class StudentMapperTest {
 		System.out.println("After All Test");
 	}
 
+	@BeforeEach
+	void setUp() {
+		System.out.println("Starting TestMethod");
+		mapper = new StudentMapper();
+	}
+
+	@AfterEach
+	void tearDown() {
+		System.out.println("Finished TestMethod");
+	}
+
 	@Test
-	public void testMethod1() {
-		System.out.println("Testing Method 1");
+	public void shouldMapStudentDtoToStudent() {
+		StudentDto dto = new StudentDto("John",
+										"Doe",
+										"john@mail.com",
+										1);
+		Student student = mapper.toStudent(dto);
+		assertEquals(dto.firstName(), student.getFirstName());
+		assertEquals(dto.lastName(), student.getLastName());
+		assertEquals(dto.email(), student.getEmail());
+		assertEquals(dto.schoolId(), student.getSchool().getId());
+		assertNotNull(student.getSchool());
 
 	}
+	@Test
+	public void shouldThrowNullPointerExceptionWhenStudentDtoIsNull() {
+		var msg=assertThrows(NullPointerException.class, () -> mapper.toStudent(null));
+		assertEquals("dto is null",msg.getMessage());
+	}
+
+	@Test
+	public void shouldMapStudentToStudentResponseDto() {
+		Student student = new Student("Bharath",
+									  "S",
+									  "bharathsatheesan@gmail.com",
+									  21);
+		StudentResponseDto dto = mapper.toResponseDto(student);
+		assertEquals(student.getFirstName(), dto.firstName());
+		assertEquals(student.getLastName(), dto.lastName());
+		assertEquals(student.getEmail(), dto.email());
+
+	}
+
 
 }
